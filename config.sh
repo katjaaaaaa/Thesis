5507baae8ed273d5d2f404d203bae1bb6bf2a83e
katjaaaaaa <katkamshova@gmail.com>
2024-05-08 00:00:53 +0200

    Added the file needed to configure the job on Habrok

---
+++ config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=00:30:00
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --job-name=phi3_llm
+#SBATCH --mem=50G
+
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+source $HOME/venvs/thesis_venv/bin/activate
+
+python3 -m pip install transformers tensorflow torch
+
+python3 phi3_model_prompting.py
