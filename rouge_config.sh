8ea2519beb0ab30162e817179ac3fff69c52b61c
katjaaaaaa <katkamshova@gmail.com>
2024-05-23 13:17:32 +0200

    Updated the config file with pip line

---
+++ rouge_config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --job-name=rouge
+#SBATCH --mem=50G
+
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+source $HOME/venvs/thesis_venv/bin/activate
+
+#python3 -m pip install nltk rouge_score prepare
+
+echo 1
+python3 rouge.py 1
+echo 5
+python3 rouge.py 5
+echo 7
+python3 rouge.py 7
+echo 8
+python3 rouge.py 8
+echo 9
+python3 rouge.py 9
+echo 10
+python3 rouge.py 10
+echo 14
+python3 rouge.py 14
+echo 15
+python3 rouge.py 15
+echo 17
+python3 rouge.py 17
+echo 20
+python3 rouge.py 20
